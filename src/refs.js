5569b0b547959efe062fb3654eb7235aa076e9b6
Username-Bogdan <127837806+Bogdan-Go-It@users.noreply.github.com>
2023-08-24 19:26:55 +0300

    start3

--- src/refs.js
+++
@@ -1,7 +0,0 @@
-// const refs = {
-//   select: document.querySelector('.breed-select'),
-//   loader: document.querySelector('.loader'),
-//   error: document.querySelector('.error'),
-//   catInfo: document.querySelector('.cat-info'),
-// };
-// export { refs };
